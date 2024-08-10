bae6ba018a0feeed6a99318b62b90dc4cdb5f18e
cebem1nt <mineewarik@gmail.com>
2024-08-10 12:08:22 -0300

    Bugfixes, deleted get by index functionality, edit functionality, code improvements

--- keeper.py
+++ keeper.py
@@ -39,7 +39,7 @@ class Keeper:
     
     def set_passphrase(self, passphrase: str):
         """
-        Sets passphrase hash to hash.txt file
+        Hashes password and writes it to hash.txt
         """
         passphrase_hash = self.cs.hash_data(passphrase)
         self.fs.set_content('hash', passphrase_hash)
@@ -48,7 +48,7 @@ class Keeper:
         """
         Verifies correctness of stored passphrase with given
         """
-        original_hash = self.fs.get_content('hash')
+        original_hash = self.fs.get_content('hash').strip()
         return self.cs.verify_hash(original_hash, passphrase)
 
     def get_triplets(self):
@@ -90,7 +90,7 @@ class Keeper:
         """
         matches = []
 
-        for i, triplet in enumerate(triplets_list):
+        for triplet in triplets_list:
 
             if strict and tag == triplet[0]:
                 matches.append(triplet)
@@ -101,20 +101,55 @@ class Keeper:
 
         return matches
     
-    def get_triplet_by_index(self, triplets_list: list[tuple], index: int):
-
-        if 0 <= index < len(triplets_list):
-            return triplets_list[index]
-        
-        return None
-
     def remove_triplet(self, triplet_index: int):
+        """
+        Removes triplet by index
+        """
         self.fs.remove_from_storage(triplet_index)
 
     def reset(self):
+        """
+        Resets all the data from password manager
+        """
         self.fs.suicide()
 
+    def edit_triplet_property(self, triplets_list: list[tuple], triplet: tuple, 
+                              property: int, value: str):
+        """
+        Gets list of triplets, triplet and a property to edit as index.
+            0: Tag
+            1: Login
+            2: Password
+        """
+        edited_triplet = list(triplet)
+        edited_triplet[property] = value
+        n, l, p = edited_triplet
+        self.remove_triplet(triplets_list.index(triplet))
+        self.store_triplet(n, l, p)
+
+    def dump(self, destination: str):
+        """
+        Dumps current locker to the specified dir
+        """
+        self.fs.dump_locker(destination)
+
+    def change_locker(self, locker_dir: str):
+        """
+        Changes current locker to provided directory
+        """
+        self.fs.change_locker(locker_dir)
+
+    def get_current_locker(self):
+        """
+        Returns locker currently working with
+        """
+
+        return self.fs.get_current_locker()
+
 def auth(keeper: Keeper) -> str:
+    """
+    Default funciton for console password prompt
+    """
     while True:
         passphrase = getpass("Passphrase: ")
 
@@ -125,6 +160,9 @@ def auth(keeper: Keeper) -> str:
     
 
 def registrate() -> str:
+    """
+    Default function for registration, creating passphrase
+    """
     while True:
         passphrase = getpass("Create passphrase: ")
 
@@ -138,10 +176,8 @@ def registrate() -> str:
             print("Passphrase created")
             return passphrase
 
-def print_triplet(triplet: tuple, index: int, hidden_password=True):
+def print_triplet(triplet: tuple, hidden_password=True):
     print('')
-    if index > -1:
-        print('Index:', index)
     print('Tag:', triplet[0])
     print('Login:', triplet[1])
 
@@ -155,6 +191,7 @@ def main(args: ArgumentParser):
     try:
         if not keeper.passphrase_exist():
             passphrase = registrate()
+            keeper.set_passphrase(passphrase)
 
         else:
             passphrase = auth(keeper)
@@ -166,25 +203,26 @@ def main(args: ArgumentParser):
     triplets = keeper.get_triplets()
 
     if args.add:
-        tag, login, password = args.add
+        print("A tag is a label used to identify your password in the storage system. When you need to find a specific password and login, you can search by its tag.")
+        tag = input("Enter the tag: ")
+        login = input("Enter the login: ")
+        password = input("Enter the password: ")
 
         keeper.store_triplet(tag, login, password)
-        print(f"Succesfully stored with tag: {tag}")
+        print(f"Triplet successfully stored with the tag: {tag}")
 
     if args.list:
-        for i, triplet in enumerate(triplets):
-            print_triplet(triplet, i)
+        for triplet in triplets:
+            print_triplet(triplet)
         print('')        
 
-    if args.list_unhiden:
-        for i, triplet in enumerate(triplets):
-            print_triplet(triplet, i, hidden_password=False)
+    if args.list_shown:
+        for triplet in triplets:
+            print_triplet(triplet, hidden_password=False)
         print('')
 
-    if args.reset:
-        print('\033[31m')
-        choice = input("Are you sure you want to reset all the data? [y/N] ")
-        print('\033[0m')
+    if args.reset_locker:
+        choice = input("\033[31mAre you sure you want to reset all the data? [y/N] \033[0m")
 
         if 'y' == choice.lower():
             keeper.reset()
@@ -192,33 +230,28 @@ def main(args: ArgumentParser):
             return
 
         else:
-            print('Aboarting')
+            print('Aboarting..')
 
     if args.get_strict:
-        tag = args.get_strict 
+        tag = ' '.join(args.get_strict)
         matches = keeper.get_triplet_by_tag(triplets, tag)
 
-        for i, triplet in enumerate(matches):
-            print_triplet(triplet, i, hidden_password=False)
-
-    if args.get_easy:
-        tag = args.get_easy 
-        matches = keeper.get_triplet_by_tag(triplets, tag, False)
-
-        for i, triplet in enumerate(matches):
-            print_triplet(triplet, i, hidden_password=False) 
+        for triplet in matches:
+            print_triplet(triplet, hidden_password=False)
         print('')
 
-    if args.get_by_index:
-        index = args.get_by_index
-        triplet = keeper.get_triplet_by_index(triplets, index)
-        if triplet:
-            print_triplet(triplet, index, hidden_password=False)
-        else:
-            print(f'Could not find triplet with index {index}')
-            
+    if args.get_easy:
+        tag = ' '.join(args.get_easy)
+
+        matches = keeper.get_triplet_by_tag(triplets, tag, False)
+
+        for triplet in matches:
+            print_triplet(triplet, hidden_password=False) 
+        print('')
+
     if args.remove_by_tag:
-        tag = args.remove_by_tag
+        tag = ' '.join(args.remove_by_tag)
+
         match = keeper.get_triplet_by_tag(triplets, tag)
 
         if not len(match):
@@ -226,7 +259,7 @@ def main(args: ArgumentParser):
             return
 
         print('\033[31m')
-        print_triplet(match[0], -1, hidden_password=False) 
+        print_triplet(match[0], hidden_password=False) 
         print('\033[0m')
 
         remove = input("Delete this triplet? [y/N] ")
@@ -235,57 +268,106 @@ def main(args: ArgumentParser):
             keeper.remove_triplet(triplets.index(match[0]))
             print('Triplet deleted')
         else:
-            print('Aboarting')
+            print('Aboarting..')
 
-    if args.remove_by_index:
-        index = args.remove_by_index
-        triplet = keeper.get_triplet_by_index(triplets, index)
-
-        if not triplet:
-            print(f'Could not find triplet with index: {index}')
-            return
-
-        print('\033[31m')
-        print_triplet(triplet, -1, hidden_password=False) 
-        print('\033[0m')
-
-        remove = input("Delete this triplet? [y/N] ")
-
-        if 'y' == remove.lower():
-            keeper.remove_triplet(index)
-            print('Triplet deleted')
-        else:
-            print('Aboarting')
+    if args.edit:
+        for triplet in triplets:
+            print_triplet(triplet)
+        print('')  
         
+        while True:
+            tag = input("Enter the tag: ")
+            triplet = keeper.get_triplet_by_tag(triplets, tag)
+
+            if not len(triplet):
+                print(f"Could not find triplet with tag: {tag}")
+                cont = input("Would you like to try again? [y/N] ")
+
+                if 'y' == cont.lower():
+                    continue
+                else:
+                    print('Aboarting..')
+
+            triplet = triplet[0]
+
+            print("Parameters that can be edited: \n 0 - tag \n 1 - login \n 2 - password")
+
+            while True:
+                try:
+                    param = int(input("Enter the parameter to edit (0-2): "))
+
+                except:
+                    param = -1
+
+                if param in (0, 1, 2):
+                    break
+                else:
+                    print("Incorrect parameter")
+
+            value = input("Enter new value: ")
+
+            keeper.edit_triplet_property(triplets, triplet, int(param), value)
+            
+            print(f"Succesfully edited triplet with tag: {tag}")
+
+    if args.dump:
+        dest = args.dump
+
+        try:
+            keeper.dump(dest)
+            print(f'Succesfuly dumped current locker to : {dest}')
+
+        except:
+            print(f"Could not find destination dir: {dest}")
+
+    if args.print_locker:
+        print(f"\nCurrent locker: {keeper.get_current_locker()}\n")
+
+    if args.change_locker:
+        locker_dir = args.change_locker
+        try:
+            keeper.change_locker(locker_dir)
+            print(f'Succesfuly changed current locker to : {locker_dir}')
+        except:
+            print(f"Could not find locker dir: {locker_dir}")
+            
+        return
+
 if __name__ == '__main__':
     p = ArgumentParser(description="Keeper is a Python password manager")
 
-    p.add_argument('-a', '--add', nargs=3, metavar=('TAG', 'LOGIN', 'PASSWORD'),
-                    help='Adds a tag/password/login triplet based on params')
+    p.add_argument('-a', '--add', action='store_true',
+                    help='Interactively adds tag/login/password')
     
-    p.add_argument('-g', '--get-strict', type=str,
-                   help='Gets a tag/password/login triplet based on tag')
-    
-    p.add_argument('-ge', '--get-easy', type=str,
-                   help='Based on tag, gets all tag/password/login triplets with similar tags')
+    p.add_argument('-r', '--remove-by-tag', metavar=('<tag>'), type=str, nargs='+',
+                help='Removes a tag/password/login triplet based on tag')
 
-    p.add_argument('-gi', '--get-by-index', type=int,
-                   help='Gets a tag/password/login triplet based on index')
-    
-    p.add_argument('-r', '--remove-by-tag', type=str,
-                    help='Removes a tag/password/login triplet based on tag')
-            
-    p.add_argument('-ri', '--remove-by-index', type=int,
-                        help='Removes a tag/password/login triplet based on index')
+    p.add_argument('-e', '--edit', action='store_true',
+                    help='Interactively edits param of triplet')
 
     p.add_argument('-l', '--list', action='store_true',
                    help='Lists all stored triplets without showing password')
     
-    p.add_argument('-lu', '--list-unhiden', action='store_true',
-                    help='Lists all stored triplets with showing password')
+    p.add_argument('-ls', '--list-shown', action='store_true',
+                    help='Lists all stored triplets showing password for each one!')
 
-    p.add_argument('-reset', '--reset', action='store_true',
-                   help='Resets all data')
+    p.add_argument('-g', '--get-strict', metavar=('<tag>'), type=str, nargs='+',
+                   help='Gets a tag/password/login triplet based on tag')
+    
+    p.add_argument('-ge', '--get-easy', metavar=('<tag>'), type=str, nargs='+',
+                   help='Based on tag, gets all tag/password/login triplets with similar tags')
+        
+    p.add_argument('-du', '--dump', type=str, metavar=('<dir>'),
+                   help='Dumps current keeper storage to specified dir')
+
+    p.add_argument('-cl', '--change-locker', type=str, metavar=('<dir>'),
+                   help='Sets another directory with triplets (locker) to manipulate them. Default locker dir: ~/.keeper/default_locker.')
+
+    p.add_argument('-pl', '--print-locker', action='store_true',
+                   help='Prints directory of the current locker in use')
+
+    p.add_argument('-reset-locker', '--reset-locker', action='store_true',
+                   help='Resets all data of the current locker')
 
     args = p.parse_args()
 
--- modules/files.py
+++ modules/files.py
@@ -1,15 +1,59 @@
 import os
+from shutil import copy
 
 class FileSystem:
     """
-    Class for file manipulations related to password management.
+    Class for file manipulatoions with password manager file system
     """
+
     def __init__(self, dir='~/.keeper') -> None:
-        self.dir = os.path.expanduser(dir)
-        self.storage = os.path.join(self.dir, 'storage.txt')
-        self.hash = os.path.join(self.dir, 'hash.txt')
-        self.salt = os.path.join(self.dir, '.salt')
-        self.ensure_dirs_exist()
+        self.root_dir = os.path.expanduser(dir)
+        self.current_locker = os.path.join(self.root_dir, '.current_locker') # file with selected locker path
+
+        self.init_locker()
+
+        self.storage = os.path.join(self.locker_dir, 'storage.txt')
+        self.hash = os.path.join(self.locker_dir, '.hash')
+        self.salt = os.path.join(self.locker_dir, '.salt')
+
+        for file_path in [self.storage, self.hash, self.salt]:
+            if not os.path.exists(file_path):
+                with open(file_path, 'w'):
+                    pass
+
+    def init_locker(self) -> None:
+        """
+        Ensure that required directories and files exist.
+        """
+        # making all the basic dirs
+        os.makedirs(self.root_dir, exist_ok=True)
+
+        if not os.path.exists(self.current_locker):
+            with open(self.current_locker, 'w'):
+                pass
+
+        with open(self.current_locker) as f:
+            # Reading a directory to the current locker
+            self.locker_dir = f.read().strip() 
+
+        if not self.locker_dir or not os.path.exists(self.locker_dir):
+            # if its empty, or previous setted locker doesnt exist anymore we set default one and make it 
+            self.locker_dir = os.path.join(self.root_dir, 'default_locker')
+            os.makedirs(self.locker_dir, exist_ok=True)
+
+            self.change_locker(self.locker_dir)
+
+    def dump_locker(self, dir: str) -> None:
+        """
+        Recieves destination dir and dumps current locker there
+        """
+        dir = os.path.expanduser(dir)
+
+        if dir == '.':
+            dir = os.getcwd()
+
+        for file in [self.hash, self.salt, self.storage]:
+            copy(file, dir)
 
     def hash_exists(self) -> bool:
         """
@@ -22,22 +66,14 @@ class FileSystem:
         except:
             return False
 
-    def ensure_dirs_exist(self) -> None:
-        """Ensure that required directories and files exist."""
-        os.makedirs(self.dir, exist_ok=True)
-
-        for file_path in [self.storage, self.hash, self.salt]:
-            if not os.path.exists(file_path):
-                with open(file_path, 'w'):
-                    pass
 
     def get_content(self, key: str) -> str | bytes:
         """
         Get content from a specified file.
         """
         methods = {
-            'hash': lambda: open(self.hash, 'r'),
-            'storage': lambda: open(self.storage, 'r'),
+            'hash': lambda: open(self.hash),
+            'storage': lambda: open(self.storage),
             'salt': lambda: open(self.salt, 'rb'),
         }
 
@@ -55,13 +91,15 @@ class FileSystem:
         methods = {
             'hash': lambda: open(self.hash, 'w'),
             'storage': lambda: open(self.storage, 'a'),
-            'salt': lambda: open(self.salt, 'wb')
+            'salt': lambda: open(self.salt, 'wb'),
         }
 
         if key in methods:
             with methods[key]() as f:
-                f.write(content + '\n')
-
+                if isinstance(content, bytes):
+                    f.write(content)
+                else:
+                    f.write(content + '\n')
         else:
             raise KeyError(f"Key: {key} not found")
 
@@ -86,6 +124,37 @@ class FileSystem:
         except:
             raise FileNotFoundError(f"File not found: {self.storage}")
         
+    def change_locker(self, dest: str) -> None:
+        """
+        A function to change current locker
+        """
+        dest = os.path.expanduser(dest)
+        cwd = os.getcwd()
+        
+        if not os.path.exists(dest):
+            local = os.path.join(self.root_dir, dest)
+
+            if os.path.exists(local):
+                with open(self.current_locker, 'w') as f:
+                    f.write(local)
+                return
+            else:
+                raise FileNotFoundError(f"Could not find locker in {dest}")
+
+        if dest == '.':
+            dest == cwd
+            
+        else:
+            dest = os.path.join(cwd, dest)
+        
+
+        with open(self.current_locker, 'w') as f:
+            f.write(dest)
+
+    def get_current_locker(self):
+        with open(self.current_locker) as f:
+            return f.read()
+
     def suicide(self):
         os.remove(self.hash)
         os.remove(self.storage)
